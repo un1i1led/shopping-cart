877af5014a6264034608dc5dc06f927520f2bc91
gil <gsqwow@gmail.com>
2023-03-08 20:14:27 -0700

    Add plant imgs link and their info

---
+++ src/components/imgLink.js
@@ -0,0 +1,31 @@
+import bigLeaf from '../img/bigLeaf.jpg';
+import darkPot from '../img/darkPot.jpg';
+import holeyPlant from '../img/holeyPlant.jpg';
+import largeLeaf from '../img/largeLeaf.jpg';
+import longLeaf from '../img/longLeaf.jpg';
+import thinPot from '../img/thinPot.jpg';
+import whiteRocks from '../img/whiteRocks.jpg';
+import yellowPot from '../img/yellowPot.jpg';
+
+const imgs = [
+        {link: bigLeaf, name: 'Big Leaf', id:'plant1'},
+        {link: darkPot, name: 'Dark Pot', id:'plant2'},
+        {link: holeyPlant, name: 'Holey Plant', id:'plant3'},
+        {link: largeLeaf, name: 'Large Leaf', id:'plant4'},
+        {link: longLeaf, name: 'Long Leaf', id:'plant5'},
+        {link: thinPot, name: 'Thin Pot', id:'plant6'},
+        {link: whiteRocks, name: 'White Rocks', id:'plant7'},
+        {link: yellowPot, name: 'Yellow Pot', id:'plant8'}
+    ];
+
+export {
+    bigLeaf,
+    darkPot,
+    holeyPlant,
+    largeLeaf,
+    longLeaf,
+    thinPot,
+    whiteRocks,
+    yellowPot,
+    imgs
+}
