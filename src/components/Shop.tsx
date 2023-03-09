3ec88fdfcb670cbfe834c05aae7551405eed15f0
gil <gsqwow@gmail.com>
2023-03-08 20:13:34 -0700

    Add plant cards and their dynamic route

--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -1,7 +1,30 @@
+import Card from './Card';
+import { Link } from 'react-router-dom';
+import { bigLeaf, darkPot, holeyPlant, largeLeaf, longLeaf, thinPot, whiteRocks, yellowPot } from './imgLink';
+import { imgs } from './imgLink';
+
 const Shop = () => {
+
+    const addCards = () => {
+        return (
+            imgs.map((data, key) => {
+                const icon = imgs[key].link
+                return (
+                    <Link to={`/shop/${imgs[key].id}`} className='card-link' key={key}>
+                        <Card key={key} icon={icon} name={imgs[key].name}/>
+                    </Link>
+                )
+            })
+        )
+    }
+
     return (
         <div className='shop'>
-            <h1>Shop page.</h1>
+            <div className='shop-main'>
+                <div className='items-div'>
+                    {addCards()}
+                </div>
+            </div>
         </div>
     )
 }
