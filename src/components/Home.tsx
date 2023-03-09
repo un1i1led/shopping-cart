c342c9fbe3e7953f8ed43aaffe6ade0aff13b8d6
gil <gsqwow@gmail.com>
2023-03-08 20:13:00 -0700

    Add h1 & shop btn to home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,17 @@
+import { Link } from 'react-router-dom'; 
+
 const Home = () => {
     return (
         <div className='home'>
-            <h1>Home Page.</h1>
+            <div className='home-left'></div>
+            <div className='home-right'>
+                <h1 className='header-h1'>Plantify</h1>
+                <div className='btn-div'>
+                    <Link to='/shop'>
+                        <button className='btn-big'>S&nbsp;h&nbsp;o&nbsp;p</button>
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 }
