66a7617f86e54477a59be056e564fde0ea4fa66a
gil <gsqwow@gmail.com>
2023-03-10 21:04:05 -0700

    Add description and cost to plants

--- src/components/imgLink.js
+++ src/components/imgLink.js
@@ -11,21 +11,21 @@ import minus from '../img/minus.png';
 import cart from '../img/cart.png';
 
 const imgs = [
-        {link: bigLeaf, name: 'Big Leaf', id:'plant1', 
+        {link: bigLeaf, name: 'Big Leaf', id:'plant1', cost: 27,
         description: 'The Big Leaf plant is a stunning foliage plant that boasts large, lush leaves that are the envy of other plants.'},
-        {link: darkPot, name: 'Dark Pot', id:'plant2', 
+        {link: darkPot, name: 'Dark Pot', id:'plant2', cost: 14,
         description: 'Ficus cyathistipula is a popular houseplant due to its attractive foliage and ease of care. It prefers bright, indirect light and moderate humidity, but can tolerate lower light levels and dry air.'},
-        {link: holeyPlant, name: 'Holey Plant', id:'plant3',
+        {link: holeyPlant, name: 'Holey Plant', id:'plant3', cost: 60,
         description: 'The Holey Plant is a unique and intriguing addition to any plant collection. This plant is known for its unusual leaves, which are covered in small, round holes that give them a striking appearance.'},
-        {link: largeLeaf, name: 'Large Leaf', id:'plant4', 
+        {link: largeLeaf, name: 'Large Leaf', id:'plant4', cost: 12,
         description: 'The Large Leaf plant is a fast-growing plant that can reach up to 6 feet in height in just a few years, making it an excellent choice for creating a dramatic focal point in a garden or indoor space.'},
-        {link: longLeaf, name: 'Long Leaf', id:'plant5', 
+        {link: longLeaf, name: 'Long Leaf', id:'plant5', cost: 44,
         description: 'The Long Leaf plant grows into a graceful and slender shrub that can reach up to 6 feet in height. The stems of the plant are thin and wiry, and the leaves grow in a staggered pattern, giving the plant a slightly asymmetrical appearance.'},
-        {link: thinPot, name: 'Thin Pot', id:'plant6',
+        {link: thinPot, name: 'Thin Pot', id:'plant6', cost: 62,
         description: 'Aspidistra elatior is a slow-growing plant that can take several years to reach its mature size of 2-3 feet in height and width. It is a popular choice for indoor environments due to its tolerance of low light levels and infrequent watering.'},
-        {link: whiteRocks, name: 'White Rocks', id:'plant7',
+        {link: whiteRocks, name: 'White Rocks', id:'plant7', cost: 45,
         description: 'Strelitzia Nicolai is a popular choice for indoor and outdoor landscaping due to its impressive size and striking appearance. It is often used as a focal point in a garden or as a statement plant in a large indoor space. Its tropical look and easy-care nature make it a popular choice for anyone looking to add a touch of the exotic to their home or garden.'},
-        {link: yellowPot, name: 'Yellow Pot', id:'plant8',
+        {link: yellowPot, name: 'Yellow Pot', id:'plant8', cost: 8,
         description: 'Dracaena plants are an excellent choice for beginner gardeners or anyone looking for a low-maintenance, attractive houseplant. They are versatile, easy to care for, and can help improve indoor air quality.'}
     ];
 
